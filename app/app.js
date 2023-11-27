be8c00ddbca55ddfe3833d79538d0b4d8991b79f
hochituong7 <hochituong7@gmail.com>
2023-11-27 20:06:59 +0700

    setup swagger

--- app/app.js
+++ app/app.js
@@ -5,6 +5,10 @@ const userRouter = require("../router/userRouter")
 const bookRouter = require("../router/bookRouter")
 const authorRouter = require("../router/authorRouter")
 const { connect } = require("../db/db")
+
+const swaggerUi = require("swagger-ui-express")
+const document = require("../config/swaggerOptions")
+
 // use middleware to form our contract for incoming json payload ONLY !!!
 app.use(express.json())
 
@@ -33,6 +37,10 @@ app.get("/", (req, res, next) => {
 app.use("/users", userRouter)
 app.use("/books", bookRouter)
 app.use("/authors", authorRouter)
+
+// use middleware for api-docs swagger\
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(document))
+
 // bad url or error we can handle
 // with middleware
 app.use((req, res, next) => {
