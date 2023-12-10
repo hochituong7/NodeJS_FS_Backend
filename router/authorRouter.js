dcc4a7be5f2d1438a5bf429c3ab15e0f276d2bf1
hochituong7 <hochituong7@gmail.com>
2023-12-10 14:19:09 +0700

    swagger API setting

--- router/authorRouter.js
+++ router/authorRouter.js
@@ -12,7 +12,7 @@ const auth = require("../auth/authorization")
 router.get("/", [auth, getAllAuthors])
 router.get("/:authorId", [auth, getAuthorByIds])
 router.post("/", [auth, postAuthor])
-router.put("/:authorId", [auth, updateAuthor])
+router.patch("/:authorId", [auth, updateAuthor])
 router.delete("/:authorId", [auth, deleteAuthor])
 
 module.exports = router
